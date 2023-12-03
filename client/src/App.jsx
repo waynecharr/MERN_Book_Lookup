d05e9c123c8e0585b3ba686189b379d0f858061f
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-12-02 23:10:58 -0500

    updated the apps.jsx file to use the apollo provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,36 @@
+import React from 'react';
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Outlet />
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Outlet />
+//     </>
+//   );
+// }
+
 
 export default App;
